58938a4f1d50ba2e5e9022fc2aa88d16f1f5c97b
Ansu26 <dimar_suarez82182@elpoli.edu.co>
2021-12-20 15:46:00 -0500

    Cuarto commit. Código optimizado.

--- src/retosoftka/RetoSoftka.java
+++ src/retosoftka/RetoSoftka.java
@@ -7,15 +7,17 @@ import javax.swing.JOptionPane;
 
 
 public class RetoSoftka {
+    
     static Scanner lectura= new Scanner(System.in);
     static String opcion = "1";
     static String nombre;
+    static int puntuacion=0,nivel=1;
+    
     public static void main(String[] args) {
         //Declaración de variables
         Respuestas res[];
         Preguntas preg = new Preguntas();
         
-        int puntuacion=0,nivel=1;
         Random numAleatorio = new Random();
         int aleatorio;
         //Se obtienen las preguntas por nivel de la clase preguntas
@@ -36,6 +38,8 @@ public class RetoSoftka {
         //Menú
         while(!opcion.equals("0")) {
             try{
+              //Se empieza en el nivel 1 por defecto y este va subiendo si se responde correctamente.
+              //El primer switch detecta en qué nivel se encuentra y en base a esto muestra las preguntas correspondientes a este nivel.
               switch (nivel) {
                 case 1:
                     aleatorio = numAleatorio.nextInt(5);
@@ -47,54 +51,8 @@ public class RetoSoftka {
                         "\nC. "+res[2].getRespuesta()+
                         "\nD. "+res[3].getRespuesta()+
                         "\n0.Salir");
-                    switch (opcion) {
-                        case "A":
-                            if (res[0].isCorrecta()){
-                                JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 1000 puntos");
-                                puntuacion=puntuacion+1000;
-                                nivel++;
-                            }
-                            else{
-                                JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
-                                opcion="0";
-                            }
-                            break;
-                        case "B":
-                            if (res[1].isCorrecta()){
-                                JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 1000 puntos");
-                                puntuacion = puntuacion+1000;
-                                nivel++;
-                            }
-                            else{
-                                JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
-                                opcion = "0";
-                            }
-                            break;
-                        case "C":
-                            if (res[2].isCorrecta()){
-                                JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 1000 puntos");
-                                puntuacion = puntuacion+1000;
-                                nivel++;
-                            }
-                            else{
-                                JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
-                                opcion = "0";
-                            }
-                            break;
-                        case "D":
-                            if (res[3].isCorrecta()){
-                                JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 1000 puntos");
-                                puntuacion = puntuacion+1000;
-                                nivel++;
-                            }
-                            else{
-                                JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
-                                opcion = "0";
-                            }
-                            break;
-                        default:
-                            break;
-                    }
+                    //Se llama el método que verifica si la opcion seleccionada es la correcta o no.
+                    correcta(res);
                     break;
             case 2:
                 aleatorio = numAleatorio.nextInt(5);
@@ -106,54 +64,7 @@ public class RetoSoftka {
                     "\nC. "+res[2].getRespuesta()+
                     "\nD. "+res[3].getRespuesta()+
                     "\n0.Salir");
-                switch (opcion) {
-                    case "A":
-                        if (res[0].isCorrecta()){
-                            JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!.¿¿ Ganaste 2000 puntos");
-                            puntuacion = puntuacion+2000;
-                            nivel++;
-                        }
-                        else{
-                            JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
-                            opcion = "0";
-                        }
-                        break;
-                    case "B":
-                        if (res[1].isCorrecta()){
-                            JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 2000 puntos");
-                            puntuacion = puntuacion+2000;
-                            nivel++;
-                        }
-                        else{
-                            JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
-                            opcion = "0";
-                        }
-                        break;
-                    case "C":
-                        if (res[2].isCorrecta()){
-                            JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!,¿. Ganaste 2000 puntos");
-                            puntuacion = puntuacion+2000;
-                            nivel++;
-                        }
-                        else{
-                            JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
-                            opcion = "0";
-                        }
-                        break;
-                    case "D":
-                        if (res[3].isCorrecta()){
-                            JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 2000 puntos");
-                            puntuacion = puntuacion+2000;
-                            nivel++;
-                        }
-                        else{
-                            JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
-                            opcion = "0";
-                        }
-                        break;
-                    default:
-                        break;
-                }
+                correcta(res);
                 break;
             case 3:
                 aleatorio = numAleatorio.nextInt(5);
@@ -165,54 +76,7 @@ public class RetoSoftka {
                     "\nC. "+res[2].getRespuesta()+
                     "\nD. "+res[3].getRespuesta()+
                     "\n0.Salir");
-                switch (opcion) {
-                    case "A":
-                        if (res[0].isCorrecta()){
-                            JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 3000 puntos");
-                            puntuacion=puntuacion+3000;
-                            nivel++;
-                        }
-                        else{
-                            JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
-                            opcion = "0";
-                        }
-                        break;
-                    case "B":
-                        if (res[1].isCorrecta()){
-                            JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 3000 puntos");
-                            puntuacion = puntuacion+3000;
-                            nivel++;
-                        }
-                        else{
-                            JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
-                            opcion = "0";
-                        }
-                        break;
-                    case "C":
-                        if (res[2].isCorrecta()){
-                            JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 3000 puntos");
-                            puntuacion = puntuacion+3000;
-                            nivel++;
-                        }
-                        else{
-                            JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
-                            opcion = "0";
-                        }
-                        break;
-                    case "D":
-                        if (res[3].isCorrecta()){
-                            JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 3000 puntos");
-                            puntuacion = puntuacion+3000;
-                            nivel++;
-                        }
-                        else{
-                            JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
-                            opcion = "0";
-                        }
-                        break;
-                    default:
-                        break;
-                }
+                correcta(res);
                 break;
             case 4:
                 aleatorio = numAleatorio.nextInt(5);
@@ -224,54 +88,7 @@ public class RetoSoftka {
                     "\nC. "+res[2].getRespuesta()+
                     "\nD. "+res[3].getRespuesta()+
                     "\n0.Salir");
-                switch (opcion) {
-                    case "A":
-                        if (res[0].isCorrecta()){
-                            JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 4000 puntos");
-                            puntuacion = puntuacion+4000;
-                            nivel++;
-                        }
-                        else{
-                            JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
-                            opcion = "0";
-                        }
-                        break;
-                    case "B":
-                        if (res[1].isCorrecta()){
-                            JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 4000 puntos");
-                            puntuacion = puntuacion+4000;
-                            nivel++;
-                        }
-                        else{
-                            JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
-                            opcion = "0"; 
-                        }
-                        break;
-                    case "C":
-                        if (res[2].isCorrecta()){
-                            JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 4000 puntos");
-                            puntuacion = puntuacion+4000;
-                            nivel++;
-                        }
-                        else{
-                            JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
-                            opcion = "0";
-                        }
-                        break;
-                    case "D":
-                        if (res[3].isCorrecta()){
-                            JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 4000 puntos");
-                            puntuacion = puntuacion+4000;
-                            nivel++;
-                        }
-                        else{
-                            JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
-                            opcion = "0";
-                        }
-                        break;
-                    default:
-                        break;
-                }
+                correcta(res);
                 break;
             case 5:
                 aleatorio = numAleatorio.nextInt(5);
@@ -283,55 +100,7 @@ public class RetoSoftka {
                     "\nC. "+res[2].getRespuesta()+
                     "\nD. "+res[3].getRespuesta()+
                     "\n0.Salir");
-                switch (opcion) {
-                    case "A":
-                        if (res[0].isCorrecta()){
-                            JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 5000 puntos");
-                            puntuacion = puntuacion+5000;
-                            nivel++;
-                        }
-                        else{
-                            JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
-                            opcion = "0";
-                        }
-                        break;
-                    case "B":
-                        if (res[1].isCorrecta()){
-                            JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 5000 puntos");
-                            puntuacion = puntuacion+5000;
-                            nivel++;
-                        }
-                        else{
-                            JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
-                            opcion = "0";
-                        }
-                        break;
-                    case "C":
-                        if (res[2].isCorrecta()){
-                            JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 5000 puntos");
-                            puntuacion = puntuacion+5000;
-                            nivel++;
-                        }
-                        else{
-                            JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
-                            opcion = "0";
-                        }
-                        break;
-                    case "D":
-                        if (res[3].isCorrecta()){
-                            JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 5000 puntos");
-                            puntuacion=puntuacion+5000;
-                            nivel++;
-                        }
-                        else{
-                            JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
-                            opcion="0";
-                        }
-                        break;
-                    default:
-                        break;
-                }
-                
+                correcta(res);
                 break;
             case 6:
                 if (puntuacion==15000) {
@@ -347,6 +116,83 @@ public class RetoSoftka {
         }catch(Exception e){
             JOptionPane.showMessageDialog(null,"Opcion Incorrecta");
         }
+        }
+    }
+    
+    //Metodo para verificar si una respuesta es correcta o no.
+    static private void correcta(Respuestas res[]){
+        switch (opcion) {
+                    case "A":
+                        if (res[0].isCorrecta()){
+                            //Se calcula la puntuación dependiendo del nivel.
+                            puntuacion();
+                            nivel++;
+                        }
+                        else{
+                            JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
+                            opcion = "0";
+                        }
+                        break;
+                    case "B":
+                        if (res[1].isCorrecta()){
+                            puntuacion();
+                            nivel++;
+                        }
+                        else{
+                            JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
+                            opcion = "0";
+                        }
+                        break;
+                    case "C":
+                        if (res[2].isCorrecta()){
+                            puntuacion();
+                            nivel++;
+                        }
+                        else{
+                            JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
+                            opcion = "0";
+                        }
+                        break;
+                    case "D":
+                        if (res[3].isCorrecta()){
+                            puntuacion();
+                            nivel++;
+                        }
+                        else{
+                            JOptionPane.showMessageDialog(null,"Respuesta incorrecta. Juego Terminado");
+                            opcion="0";
+                        }
+                        break;
+                    default:
+                        break;
+        }
+    }
+    
+    //Metodo para calcular la puntuacion si una respuesta es correcta.
+    static private void puntuacion(){
+        switch (nivel) {
+            case 1:
+                JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 1000 puntos");
+                puntuacion = puntuacion+1000;
+                break;
+            case 2:
+                JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 2000 puntos");
+                puntuacion = puntuacion+2000;
+                break;
+            case 3:
+                JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 3000 puntos");
+                puntuacion = puntuacion+3000;
+                break;
+            case 4:
+                JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 4000 puntos");
+                puntuacion = puntuacion+4000;
+                break;
+            case 5:
+                JOptionPane.showMessageDialog(null,"¡Respuesta Correcta!. Ganaste 5000 puntos");
+                puntuacion = puntuacion+5000;
+                break;
+            default:
+                throw new AssertionError();
         }
     }
 }
